08669f89e3936410f4bd0cfaca817c8370e17fe3
Cristhian Castiblanco <criscast2602@gmail.com>
2023-05-16 03:09:04 -0500

    Register complete and login validation

--- app.js
+++ app.js
@@ -36,17 +36,41 @@ app.get('/register', (req, res)=>{
 app.post('/register', async (req, res)=>{
     const user = req.body.typeNameX
     const name = req.body.typeEmailX
-    const pass = req.body.typeRePassX
+    const pass = req.body.typePassX
     let passhash = await bcryptjs.hash(pass, 8)
-    connection.query('INSERT INTO users SET ?', {user:user, name:name, rol:'Estudiante', pass:passhash}, async(err, result)=>{
-        if(error){
-            console.log(error)
+    connection.query(`INSERT INTO users (user, name, rol, pass) VALUES ('${user}','${name}','Estudiante','${passhash}')` , async(err, result)=>{
+    if(err){
+            console.log(err)
         }
         else{
-            res.send('Registro realizado')
+            res.render('login.ejs')
         }
     })
 })
+
+app.post('/login',async (req, res)=>{
+    const user = req.body.EmailX
+    const pass = req.body.PasswordX
+    var contraseña = false
+    var rol = " "
+    connection.query(`SELECT  pass, rol FROM users WHERE user = '${user}'`, async(err, result)=>{
+    bcryptjs.compare(pass, result[0].pass, (err,resul)=>{
+        if(resul){
+            if(result[0].rol == "Estudiante")
+            res.render('vista.estudiante.ejs')
+            if(result[0].rol == "Administrador")
+            res.render('vista.administrador.ejs')
+            if(result[0].rol == "Profesor")
+            res.render('vista.profesor.ejs')
+        }else{
+            res.render('login.ejs')
+        }
+    })
+    })
+
+})
+
+
 
 app.listen(3000, (req, res)=>{
     console.log('Server is running on http://localhost:3000')
